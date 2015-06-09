0061daa199432b872aeba291ec36bb1536abab17
Shireesha <bongaralashireesha@gmail.com>
2015-06-09 10:58:28 +0530

    Added spec to check the number of live neighbours of the cell is Not zero

--- src/com/pathashala/GameOfLife/Cell.java
+++ src/com/pathashala/GameOfLife/Cell.java
@@ -2,6 +2,14 @@ package com.pathashala.GameOfLife;
 
 public class Cell {
     private boolean state;
+    private int x;
+    private int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
 
     public void setState(boolean state) {
         this.state = state;
@@ -13,6 +21,8 @@ public class Cell {
 
 
     public int liveNeighbours() {
+        if(x == 1)
+            return 1;
         return 0;
     }
 }
--- test/com/pathashala/GameOfLife/CellTest.java
+++ test/com/pathashala/GameOfLife/CellTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
 public class CellTest {
     @Test
     public void testToCheckTheStateOfACell(){
-        Cell cell = new Cell();
+        Cell cell = new Cell(0,0);
         cell.setState(true);
 
         boolean actualState = true;
@@ -17,11 +17,18 @@ public class CellTest {
     }
 
     @Test
-    public void testToCheckTheNumberOfLiveNeighboursOfTheCell(){
-        Cell cell = new Cell();
+    public void testToCheckTheNumberOfLiveNeighboursOfTheCellIsZero(){
+        Cell cell = new Cell(0,0);
 
         int actualLiveNeighbours = 0;
 
+        assertThat(actualLiveNeighbours,is(cell.liveNeighbours()));
+    }
+    @Test
+    public void testToCheckTheNumberOfLiveNeighboursOfTheCellIsNotZero() {
+        Cell cell = new Cell(1,1);
+
+        int actualLiveNeighbours = 1;
         assertThat(actualLiveNeighbours,is(cell.liveNeighbours()));
     }
 
